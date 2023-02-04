0566be31124bba7921495b1ceb7c00c678fe555c
RadkoCernohous <radko.cernohous.jr@seznam.cz>
2023-02-04 20:12:00 +0100

    x

--- sin2023-cernohous/logout.php
+++ sin2023-cernohous/logout.php
@@ -7,28 +7,27 @@ $url = "http://127.0.0.1/cernohous/sin2023-cernohous/index.php"; //Změnit URL
 session_start();
 $data = $_POST["schovanyKontejner"];
 
-if(strcmp($_SESSION["loginData"],$data) != 0){
-    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-    $conn = mysqli_connect($serverIP, $username, $dtbsPassword, $dtbsName);
-    $conn->set_charset("utf8mb4");
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+$conn = mysqli_connect($serverIP, $username, $dtbsPassword, $dtbsName);
+$conn->set_charset("utf8mb4");
 
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-      }
-
-    $sql = 'UPDATE users SET userdata = ? WHERE email= ?';   
-    $email =$_SESSION["logMail"];
-    
-    
-    $statement = $conn->prepare($sql);
-    $statement->bind_param("ss",$data,$email);
-    if($statement->execute()){
-    //echo "data succesfully saved";
-    }
-    mysqli_close($conn);
-    session_destroy();
-
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
 }
-  header("Location: ".$url); 
+
+$sql = 'UPDATE users SET userdata = ? WHERE email= ?';   
+$email =$_SESSION["logMail"];
+    
+    
+$statement = $conn->prepare($sql);
+$statement->bind_param("ss",$data,$email);
+if($statement->execute()){
+  //echo "data succesfully saved";
+  }
+mysqli_close($conn);
+session_destroy();
+
+
+header("Location: ".$url); 
 
 ?>
--- sin2023-cernohous/updatedtbs.php
+++ sin2023-cernohous/updatedtbs.php
@@ -7,26 +7,23 @@ session_start();
 $data = $_POST["schovanyKontejner3"];
 
 
-if(strcmp($_SESSION["loginData"],$data) != 0){
-    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-    $conn = mysqli_connect($serverIP, $username, $dtbsPassword, $dtbsName);
-    $conn->set_charset("utf8mb4");
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+$conn = mysqli_connect($serverIP, $username, $dtbsPassword, $dtbsName);
+$conn->set_charset("utf8mb4");
 
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-      }
-
-    $sql = 'UPDATE users SET userdata = ? WHERE email= ?';   
-    $email =$_SESSION["logMail"];
-    
-    
-    $statement = $conn->prepare($sql);
-    $statement->bind_param("ss",$data,$email);
-    if($statement->execute()){
-    echo "data succesfully saved";
-    }
-    mysqli_close($conn);
-
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
 }
+
+$sql = 'UPDATE users SET userdata = ? WHERE email= ?';   
+$email =$_SESSION["logMail"];
+    
+    
+$statement = $conn->prepare($sql);
+$statement->bind_param("ss",$data,$email);
+if($statement->execute()){
+  echo "data succesfully saved";
+}
+mysqli_close($conn);
 
 ?>
